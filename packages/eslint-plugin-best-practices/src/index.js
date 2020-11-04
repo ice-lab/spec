6348e51faac64cf755910e177b3e63bfacccec81
luhc228 <luhengchang228@gmail.com>
2020-11-04 17:49:25 +0800

    fix: lint

--- packages/eslint-plugin-best-practices/src/index.js
+++ packages/eslint-plugin-best-practices/src/index.js
@@ -1,13 +1,13 @@
 const path = require('path');
-const requireAll = require('require-all')
+const requireAll = require('require-all');
 
 exports.rules = requireAll({
   dirname: path.resolve(__dirname, 'rules'),
-})
+});
 
 exports.configs = requireAll({
   dirname: path.resolve(__dirname, 'configs'),
-})
+});
 
 exports.processors = {
   '.json': {
