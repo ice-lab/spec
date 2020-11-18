2a4ebf89c0e77c47804d6b46dc2202a1d6e065ec
云墨 <lg208143@alibaba-inc.com>
2020-11-18 18:10:04 +0800

    fix: Eslint says all enums in Typescript app are “already declared in the upper scope”

--- packages/spec/src/eslint/react-ts.js
+++ packages/spec/src/eslint/react-ts.js
@@ -6,4 +6,8 @@ module.exports = {
     // For some ci and jest test env, we chose require.resolve instead 'plugin:@iceworks/best-practices/react-ts'
     require.resolve('@iceworks/eslint-plugin-best-practices/src/configs/react-ts'),
   ],
+  rules: {
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+  },
 };
