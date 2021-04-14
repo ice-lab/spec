3ca4f69e06cb3f850db6885b70fbc0013962b404
yangfan <18767120422@163.com>
2021-04-14 14:01:01 +0800

    feat: update to eslint 7.x

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,10 @@ const { getESLintConfig } = require('./packages/spec/src/');
 
 module.exports = getESLintConfig('react', {
   env: {
-    jest: true
+    jest: true,
   },
   rules: {
     // For test file. This project is no UI project, not use line height.
-    '@iceworks/best-practices/recommend-add-line-height-unit': 'off'
-  }
+    '@iceworks/best-practices/recommend-add-line-height-unit': 'off',
+  },
 });
