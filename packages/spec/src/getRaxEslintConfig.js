0cb12642d664d8bb525fd14b37ef46f3bc0c1310
yangfan <18767120422@163.com>
2021-08-25 10:59:15 +0800

    feat: update review

--- packages/spec/src/getRaxEslintConfig.js
+++ packages/spec/src/getRaxEslintConfig.js
@@ -1,37 +1,36 @@
 const fs = require('fs');
 const path = require('path');
+const JSON5 = require('json5');
 const deepmerge = require('./deepmerge');
 
 // Add specific rules for rax compile-time miniapp
 module.exports = function (config) {
   try {
-    const buildConfig = JSON.parse(
-      fs.readFileSync(
-        path.join(process.cwd(), 'build.json'),
-        'utf8',
-      ),
-    );
+    const buildConfigFilePath = path.join(process.cwd(), 'build.json');
 
-    const isCompileTime = (target) => (
-      buildConfig.targets.find((t) => t === target) &&
-      buildConfig[target] && buildConfig[target].buildType === 'compile'
-    );
+    if (fs.existsSync(buildConfigFilePath)) {
 
-    // At present, only miniapp and wechat-miniprogram support build for compile-time
-    if (isCompileTime('miniapp') || isCompileTime('wechat-miniprogram')) {
-      // https://www.npmjs.com/package/eslint-plugin-rax-compile-time-miniapp
-      const { configs } = require('eslint-plugin-rax-compile-time-miniapp');
+      const buildConfig = JSON5.parse(fs.readFileSync(buildConfigFilePath, 'utf8'));
 
-      if (!config.plugins) {
-        config.plugins = [];
+      const isCompileTime = (target) => (
+        buildConfig.targets.find((t) => t === target) &&
+        buildConfig[target] && buildConfig[target].buildType === 'compile'
+      );
+
+      // At present, only miniapp and wechat-miniprogram support build for compile-time
+      if (isCompileTime('miniapp') || isCompileTime('wechat-miniprogram')) {
+        // https://www.npmjs.com/package/eslint-plugin-rax-compile-time-miniapp
+        const { configs } = require('eslint-plugin-rax-compile-time-miniapp');
+
+        config.plugins = config.plugins || [];
+
+        config.plugins.push('rax-compile-time-miniapp');
+        // For some ci and jest test env, we chose set config instead 'plugin:rax-compile-time-miniapp/recommended'
+        config.rules = deepmerge(config.rules || {}, configs.recommended.rules);
       }
-
-      config.plugins.push('rax-compile-time-miniapp');
-      // For some ci and jest test env, we chose set config instead 'plugin:rax-compile-time-miniapp/recommended'
-      config.rules = deepmerge(config.rules || {}, configs.recommended.rules);
     }
   } catch (e) {
-    // ignore
+    console.log('Add specific rules for rax compile-time miniapp failed!', e);
   }
   return config;
 };
